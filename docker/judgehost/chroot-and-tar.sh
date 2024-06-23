3ca256154432b7dcae8a16f19712432281845be7
Arthur Chang <arthurc0102@gmail.com>
2024-06-23 15:40:07 +0800

    Remove C#, VB, Ruby, Swift and Golang, also upgrade to nodejs 20

--- docker/judgehost/chroot-and-tar.sh
+++ docker/judgehost/chroot-and-tar.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 # Usage: https://github.com/DOMjudge/domjudge/blob/main/misc-tools/dj_make_chroot.in#L58-L87
-/opt/domjudge/judgehost/bin/dj_make_chroot -i "ruby"
+/opt/domjudge/judgehost/bin/dj_make_chroot
 
 /opt/domjudge/judgehost/bin/dj_run_chroot "apt-get update && \
-apt-get install -y apt-transport-https dirmngr gnupg ca-certificates && \
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF && \
-echo 'deb https://download.mono-project.com/repo/debian stable-buster main' | tee /etc/apt/sources.list.d/mono-official-stable.list && \
-apt-get update && \
-apt-get install -y mono-devel mono-basic-dbg && \
+\
 apt-get install -y php-cli && \
+\
 apt-get install -y curl unzip && \
-curl -sL https://deb.nodesource.com/setup_12.x | bash - && \
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && \
 apt-get install -y nodejs && \
 apt-get remove --auto-remove -y curl unzip && \
+\
 rm -rf /var/lib/apt/lists/*"
 
 cd /
