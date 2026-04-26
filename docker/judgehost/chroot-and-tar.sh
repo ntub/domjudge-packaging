7d538b14e92222cbdab2a3a0bc66236b616c6eb4
Arthur Chang <arthurc0102@gmail.com>
2026-04-26 19:00:07 +0800

    Install mono-mcs in chroot to support C# submissions

--- docker/judgehost/chroot-and-tar.sh
+++ docker/judgehost/chroot-and-tar.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Usage: https://github.com/DOMjudge/domjudge/blob/main/misc-tools/dj_make_chroot.in#L58-L87
-/opt/domjudge/judgehost/bin/dj_make_chroot
+/opt/domjudge/judgehost/bin/dj_make_chroot -i mono-mcs
 
 cd /
 echo "[..] Compressing chroot"
